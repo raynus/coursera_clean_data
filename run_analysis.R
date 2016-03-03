e9210502029de8fa7ac5c5fd4523714e733e350d
raynus <raynus.blueray@gmail.com>
2016-03-03 09:28:37 +0700

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -17,22 +17,24 @@ test_x<-read.table("./test/x_test.txt")
 test_y<-read.table("./test/y_test.txt")
 names(test_y)<-"activity"
 names(test_x)<-feature_x_name$V2
-#test_y$activity<-as.factor(test_y$activity)
 
+
+#Merge data together
 test_data<-cbind(test_sub,test_x,test_y)
 
 ##Finish preparing data
-
 total_data<-rbind(train_data,test_data)
 total_data$activity<-as.factor(total_data$activity)
 levels(total_data$activity)<-c("walking","walking_upstairs","walking_downstairs","sitting","standing","laying")
 col_sd_mean<-c(grep("std()",names(total_data)),grep("mean()",names(total_data)))
 
-#This is a subset for sd and mean
+#This is a subset for sd and meanAt this 
 sub_col_sd_mean<-total_data[,col_sd_mean]
 
 library(reshape2)
 #Generate for each subject
 each_subj<-aggregate(.~subject,subset(total_data,select=-c(activity)),FUN=mean)
+write.csv(each_subj,"each_subj.csv")
+#Generate for each activity
 each_activitiy<-aggregate(.~activity,subset(total_data,select=-c(subject)),FUN=mean)
-
+write.csv(each_activitiy,"each_activity.csv")
